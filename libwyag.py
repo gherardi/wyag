2ee5cfbcc577abd60ea0e82aaec82b9747e4098c
Victor Gherardi <gherardivictor@gmail.com>
2025-12-15 09:59:49 +0100

    improve comments

--- libwyag.py
+++ libwyag.py
@@ -12,98 +12,79 @@ from datetime import datetime
 from fnmatch import fnmatch
 from math import ceil
 
-# argparse library documentation: https://docs.python.org/3/library/argparse.html
-# create the main parser for arguments
-argparser = argparse.ArgumentParser(description="wyag - the stupidest content tracker")
+# argparse library documentation:
+# https://docs.python.org/3/library/argparse.html
 
-# add subparser to catch commands and make it mandatory
+# main argument parser
+argparser = argparse.ArgumentParser(description="wyag - the stupidest content tracker")
 argsubparsers = argparser.add_subparsers(title="Commands", dest="command")
 argsubparsers.required = True
 
-# subparser for init command
+# init command
 argsp = argsubparsers.add_parser("init", help="Initialize a new, empty repository.")
 argsp.add_argument("path", metavar="directory", nargs="?", default=".", help="Where to create the repository.")
 
-# subparser for cat-file command
+# cat-file command
 argsp = argsubparsers.add_parser("cat-file", help="Provide content of repository objects")
 argsp.add_argument("type", metavar="type", choices=["blob", "commit", "tag", "tree"], help="Specify the type")
 argsp.add_argument("object", metavar="object", help="The object to display")
 
-# subparser for hash-object command
+# hash-object command
 argsp = argsubparsers.add_parser("hash-object", help="Compute object ID and optionally creates a blob from a file")
 argsp.add_argument("-t", metavar="type", dest="type", choices=["blob", "commit", "tag", "tree"], default="blob", help="Specify the type")
 argsp.add_argument("-w", dest="write", action="store_true", help="Actually write the object into the database")
 argsp.add_argument("path", help="Read object from <file>")
 
-# subparse for log command
+# log command
 argsp = argsubparsers.add_parser("log", help="Display history of a given commit.")
 argsp.add_argument("commit", default="HEAD", nargs="?", help="Commit to start at.")
 
-# subparse for ls-tree command
+# ls-tree command
 argsp = argsubparsers.add_parser("ls-tree", help="Pretty-print a tree object.")
 argsp.add_argument("-r", dest="recursive", action="store_true", help="Recurse into sub-trees")
 argsp.add_argument("tree", help="A tree-ish object.")
 
-# subparse for checkout command
+# checkout command
 argsp = argsubparsers.add_parser("checkout", help="Checkout a commit inside of a directory.")
 argsp.add_argument("commit", help="The commit or tree to checkout.")
 argsp.add_argument("path", help="The EMPTY directory to checkout on.")
 
-# subparser for show-ref command
+# show-ref command
 argsp = argsubparsers.add_parser("show-ref", help="List references.")
 
-# subparser for tag command
+# tag command
 argsp = argsubparsers.add_parser( "tag", help="List and create tags")
 argsp.add_argument("-a", action="store_true", dest="create_tag_object", help="Whether to create a tag object")
 argsp.add_argument("name", nargs="?", help="The new tag's name")
 argsp.add_argument("object", default="HEAD", nargs="?", help="The object the new tag will point to")
 
-# subparser for rev-parse command
+# rev-parse command
 argsp = argsubparsers.add_parser( "rev-parse", help="Parse revision (or other objects) identifiers")
 argsp.add_argument("--wyag-type", metavar="type", dest="type", choices=["blob", "commit", "tag", "tree"], default=None, help="Specify the expected type")
 argsp.add_argument("name", help="The name to parse")
 
-# process and validate commands
-def main(argv=sys.argv[1:]):
-    args = argparser.parse_args(argv)
-    
-    # these functions validate and process input before executing the actual command
-    match args.command:
-        # case "add"          : cmd_add(args)
-        case "cat-file"     : cmd_cat_file(args)
-        # case "check-ignore" : cmd_check_ignore(args)
-        case "checkout"     : cmd_checkout(args)
-        # case "commit"       : cmd_commit(args)
-        case "hash-object"  : cmd_hash_object(args)
-        case "init"         : cmd_init(args)
-        case "log"          : cmd_log(args)
-        # case "ls-files"     : cmd_ls_files(args)
-        case "ls-tree"      : cmd_ls_tree(args)
-        case "rev-parse"    : cmd_rev_parse(args)
-        # case "rm"           : cmd_rm(args)
-        case "show-ref"     : cmd_show_ref(args)
-        # case "status"       : cmd_status(args)
-        case "tag"          : cmd_tag(args)
-        case _              : print("Bad command.")
-
 class GitRepository (object):
+    """
+    Representation of a Git repository: holds worktree, gitdir, and configuration.
+    """
+
     worktree = None
     gitdir = None
     conf = None
 
     def __init__(self, path, force=False):
+        # set repository paths
         # folder structure: worktree/.git
         self.worktree = path
         self.gitdir = os.path.join(path, ".git")
 
-        # if we are not in a test mode (force), or we're not in a directory, raise an exception
+        # if we are not in force mode (force), .git must exist
         if not (force or os.path.isdir(self.gitdir)):
             raise Exception(f"Not a Git repository {path}")
         
-        # read configuration file in .git/config
+        # read the configuration file in .git/config
         self.conf = configparser.ConfigParser()
         cf = repo_file(self, "config")
-
         if cf and os.path.exists(cf):
             self.conf.read([cf])
         elif not force:
@@ -115,6 +96,9 @@ class GitRepository (object):
                 raise Exception(f"Unsupported repositoryformatversion: {vers}")
 
 class GitObject (object):
+    """
+    Base class for Git objects (blobs, trees, commits, tags).
+    """
 
     def __init__(self, data=None):
         if data != None:
@@ -123,25 +107,26 @@ class GitObject (object):
             self.init()
 
     def serialize(self, repo):
-        """This function MUST be implemented by subclasses.
-
-It must read the object's contents from self.data, a byte string, and
-do whatever it takes to convert it into a meaningful representation.
-What exactly that means depend on each subclass.
-
+        """
+        Serializes the object in bytes (implemented by subclasses).
         """
         raise Exception("Unimplemented!")
 
     def deserialize(self, data):
+        """
+        Deserializes data in memory (implemented by subclasses).
+        """
         raise Exception("Unimplemented!")
 
     def init(self):
-        pass # Just do nothing. This is a reasonable default!
+        # default initialization
+        pass
 
 class GitBlob(GitObject):
     fmt=b'blob'
 
     def serialize(self):
+        # a blob is already a raw data
         return self.blobdata
 
     def deserialize(self, data):
@@ -151,15 +136,22 @@ class GitCommit(GitObject):
     fmt=b'commit'
 
     def deserialize(self, data):
+        # parsing the key-value fields of the commit and tags
         self.kvlm = kvlm_parse(data)
 
     def serialize(self):
+        # serialization of the key-value dictionary
         return kvlm_serialize(self.kvlm)
 
     def init(self):
+        # initialize empty dictionary for new commit
         self.kvlm = dict()
 
 class GitTreeLeaf (object):
+    """
+    Represents an entry in a Git tree: (mode, path, sha)
+    """
+
     def __init__(self, mode, path, sha):
         self.mode = mode
         self.path = path
@@ -168,10 +160,84 @@ class GitTreeLeaf (object):
 class GitTag(GitCommit):
     fmt = b'tag'
 
-# utility to compute missing directory structure if needed
-# *path catch the remaining parameters
+def cmd_init(args):
+    repo_create(args.path)
+
+def cmd_cat_file(args):
+    repo = repo_find()
+    cat_file(repo, args.object, fmt=args.type.encode())
+
+def cmd_log(args):
+    repo = repo_find()
+
+    print("digraph wyaglog{")
+    print("  node[shape=rect]")
+    log_graphviz(repo, object_find(repo, args.commit), set())
+    print("}")
+
+def cmd_hash_object(args):
+    if args.write:
+        repo = repo_find()
+    else:
+        repo = None
+
+    with open(args.path, "rb") as fd:
+        sha = object_hash(fd, args.type.encode(), repo)
+        print(sha)
+
+def cmd_ls_tree(args):
+    repo = repo_find()
+    ls_tree(repo, args.tree, args.recursive)
+
+def cmd_checkout(args):
+    repo = repo_find()
+
+    obj = object_read(repo, object_find(repo, args.commit))
+
+    # If the object is a commit, we grab its tree
+    if obj.fmt == b'commit':
+        obj = object_read(repo, obj.kvlm[b'tree'].decode("ascii"))
+
+    # Verify that path is an empty directory
+    if os.path.exists(args.path):
+        if not os.path.isdir(args.path):
+            raise Exception(f"Not a directory {args.path}!")
+        if os.listdir(args.path):
+            raise Exception(f"Not empty {args.path}!")
+    else:
+        os.makedirs(args.path)
+
+    tree_checkout(repo, obj, os.path.realpath(args.path))
+
+def cmd_show_ref(args):
+    repo = repo_find()
+    refs = ref_list(repo)
+    show_ref(repo, refs, prefix="refs")
+
+def cmd_tag(args):
+    repo = repo_find()
+
+    if args.name:
+        tag_create(repo, args.name, args.object, create_tag_object = args.create_tag_object)
+    else:
+        # list all the tags
+        refs = ref_list(repo)
+        show_ref(repo, refs["tags"], with_hash=False)
+
+def cmd_rev_parse(args):
+    if args.type:
+        fmt = args.type.encode()
+    else:
+        fmt = None
+
+    repo = repo_find()
+
+    print (object_find(repo, args.name, fmt, follow=True))
+
 def repo_path(repo, *path):
-    """Compute path under repo's gitdir."""
+    # utility to compute missing directory structure if needed
+    # *path catch the remaining parameters
+    # Compute path under repo's gitdir.
     return os.path.join(repo.gitdir, *path)
 
 def repo_file(repo, *path, mkdir=False):
@@ -242,9 +308,6 @@ def repo_default_config():
     ret.set("core", "bare", "false")
 
     return ret
-
-def cmd_init(args):
-    repo_create(args.path)
 
 def repo_find(path=".", required=True):
     path = os.path.realpath(path)
@@ -407,27 +470,9 @@ def object_resolve(repo, name):
 
     return candidates
 
-    return name
-
-# cat-file command functions
-def cmd_cat_file(args):
-    repo = repo_find()
-    cat_file(repo, args.object, fmt=args.type.encode())
-
 def cat_file(repo, obj, fmt=None):
     obj = object_read(repo, object_find(repo, obj, fmt=fmt))
     sys.stdout.buffer.write(obj.serialize())
-
-# hash-object command functions
-def cmd_hash_object(args):
-    if args.write:
-        repo = repo_find()
-    else:
-        repo = None
-
-    with open(args.path, "rb") as fd:
-        sha = object_hash(fd, args.type.encode(), repo)
-        print(sha)
 
 def object_hash(fd, fmt, repo=None):
     """ Hash object, writing it to repo if provided."""
@@ -520,14 +565,6 @@ def kvlm_serialize(kvlm):
 
     return ret
 
-def cmd_log(args):
-    repo = repo_find()
-
-    print("digraph wyaglog{")
-    print("  node[shape=rect]")
-    log_graphviz(repo, object_find(repo, args.commit), set())
-    print("}")
-
 def log_graphviz(repo, sha, seen):
 
     if sha in seen:
@@ -618,10 +655,6 @@ def tree_serialize(obj):
         ret += sha.to_bytes(20, byteorder="big")
     return ret
 
-def cmd_ls_tree(args):
-    repo = repo_find()
-    ls_tree(repo, args.tree, args.recursive)
-
 def ls_tree(repo, ref, recursive=None, prefix=""):
     sha = object_find(repo, ref, fmt=b"tree")
     obj = object_read(repo, sha)
@@ -642,26 +675,6 @@ def ls_tree(repo, ref, recursive=None, prefix=""):
             print(f"{'0' * (6 - len(item.mode)) + item.mode.decode("ascii")} {type} {item.sha}\t{os.path.join(prefix, item.path)}")
         else: # This is a branch, recurse
             ls_tree(repo, item.sha, recursive, os.path.join(prefix, item.path))
-
-def cmd_checkout(args):
-    repo = repo_find()
-
-    obj = object_read(repo, object_find(repo, args.commit))
-
-    # If the object is a commit, we grab its tree
-    if obj.fmt == b'commit':
-        obj = object_read(repo, obj.kvlm[b'tree'].decode("ascii"))
-
-    # Verify that path is an empty directory
-    if os.path.exists(args.path):
-        if not os.path.isdir(args.path):
-            raise Exception(f"Not a directory {args.path}!")
-        if os.listdir(args.path):
-            raise Exception(f"Not empty {args.path}!")
-    else:
-        os.makedirs(args.path)
-
-    tree_checkout(repo, obj, os.path.realpath(args.path))
 
 def tree_checkout(repo, tree, path):
     for item in tree.items:
@@ -710,11 +723,6 @@ def ref_list(repo, path=None):
 
     return ret
 
-def cmd_show_ref(args):
-    repo = repo_find()
-    refs = ref_list(repo)
-    show_ref(repo, refs, prefix="refs")
-
 def show_ref(repo, refs, with_hash=True, prefix=""):
     if prefix:
         prefix = prefix + '/'
@@ -725,16 +733,6 @@ def show_ref(repo, refs, with_hash=True, prefix=""):
             print (f"{prefix}{k}")
         else:
             show_ref(repo, v, with_hash=with_hash, prefix=f"{prefix}{k}")
-
-def cmd_tag(args):
-    repo = repo_find()
-
-    if args.name:
-        tag_create(repo, args.name, args.object, create_tag_object = args.create_tag_object)
-    else:
-        # list all  tags
-        refs = ref_list(repo)
-        show_ref(repo, refs["tags"], with_hash=False)
 
 def tag_create(repo, name, ref, create_tag_object=False):
     # get the GitObject from the object reference
@@ -763,12 +761,25 @@ def ref_create(repo, ref_name, sha):
     with open(repo_file(repo, "refs/" + ref_name), 'w') as fp:
         fp.write(sha + "\n")
 
-def cmd_rev_parse(args):
-    if args.type:
-        fmt = args.type.encode()
-    else:
-        fmt = None
-
-    repo = repo_find()
-
-    print (object_find(repo, args.name, fmt, follow=True))
+def main(argv=sys.argv[1:]):
+    # parser and command dispatch
+    args = argparser.parse_args(argv)
+    
+    # these functions validate and process input before executing the actual command
+    match args.command:
+        # case "add"          : cmd_add(args)
+        case "cat-file"     : cmd_cat_file(args)
+        # case "check-ignore" : cmd_check_ignore(args)
+        case "checkout"     : cmd_checkout(args)
+        # case "commit"       : cmd_commit(args)
+        case "hash-object"  : cmd_hash_object(args)
+        case "init"         : cmd_init(args)
+        case "log"          : cmd_log(args)
+        # case "ls-files"     : cmd_ls_files(args)
+        case "ls-tree"      : cmd_ls_tree(args)
+        case "rev-parse"    : cmd_rev_parse(args)
+        # case "rm"           : cmd_rm(args)
+        case "show-ref"     : cmd_show_ref(args)
+        # case "status"       : cmd_status(args)
+        case "tag"          : cmd_tag(args)
+        case _              : print("Bad command.")
