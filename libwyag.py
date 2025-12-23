7aae3dcf713d0a626fb6c9ab82f10f299ce180ee
Victor Gherardi <gherardivictor@gmail.com>
2025-12-23 09:36:38 +0100

    add command to stage changes

--- libwyag.py
+++ libwyag.py
@@ -78,6 +78,10 @@ argsp = argsubparsers.add_parser("status", help = "Show the working tree status.
 argsp = argsubparsers.add_parser("rm", help="Remove files from the working tree and the index.")
 argsp.add_argument("path", nargs="+", help="Files to remove")
 
+# add command
+argsp = argsubparsers.add_parser("add", help = "Add files contents to the index.")
+argsp.add_argument("path", nargs="+", help="Files to add")
+
 class GitRepository(object):
     """
     Representation of a Git repository: holds worktree, gitdir, and configuration.
@@ -338,6 +342,10 @@ def cmd_status(_):
 def cmd_rm(args):
     repo = repo_find()
     rm(repo, args.path)
+
+def cmd_add(args):
+    repo = repo_find()
+    add(repo, args.path)
 
 def repo_path(repo, *path):
     # utility to compute missing directory structure if needed
@@ -1280,6 +1288,50 @@ def rm(repo, paths, delete=True, skip_missing=False):
 
     # Update the list of entries in the index, and write it back.
     index.entries = kept_entries
+    index_write(repo, index)
+
+def add(repo, paths, delete=True, skip_missing=False):
+
+    # First remove all paths from the index, if they exist.
+    rm (repo, paths, delete=False, skip_missing=True)
+
+    worktree = repo.worktree + os.sep
+
+    # Convert the paths to pairs: (absolute, relative_to_worktree).
+    # Also delete them from the index if they're present.
+    clean_paths = set()
+    for path in paths:
+        abspath = os.path.abspath(path)
+        if not (abspath.startswith(worktree) and os.path.isfile(abspath)):
+            raise Exception(f"Not a file, or outside the worktree: {paths}")
+        relpath = os.path.relpath(abspath, repo.worktree)
+        clean_paths.add((abspath,  relpath))
+
+    # Find and read the index.  It was modified by rm.  (This isn't
+    # optimal, good enough for wyag!)
+    #
+    # @FIXME, though: we could just move the index through
+    # commands instead of reading and writing it over again.
+    index = index_read(repo)
+
+    for (abspath, relpath) in clean_paths:
+        with open(abspath, "rb") as fd:
+            sha = object_hash(fd, b"blob", repo)
+
+            stat = os.stat(abspath)
+
+            ctime_s = int(stat.st_ctime)
+            ctime_ns = stat.st_ctime_ns % 10**9
+            mtime_s = int(stat.st_mtime)
+            mtime_ns = stat.st_mtime_ns % 10**9
+
+            entry = GitIndexEntry(ctime=(ctime_s, ctime_ns), mtime=(mtime_s, mtime_ns), dev=stat.st_dev, ino=stat.st_ino,
+                                  mode_type=0b1000, mode_perms=0o644, uid=stat.st_uid, gid=stat.st_gid,
+                                  fsize=stat.st_size, sha=sha, flag_assume_valid=False,
+                                  flag_stage=False, name=relpath)
+            index.entries.append(entry)
+
+    # Write the index back
     index_write(repo, index)
 
 def main(argv=sys.argv[1:]):
